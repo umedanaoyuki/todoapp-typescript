e4cc836303a6ce3c5b6788b9016efa066fa2c3bf
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-26 16:05:09 +0900

    初期状態のTodoを作成

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,9 +1,33 @@
 import Form from "./components/Form";
 import List from "./components/List";
+import {useState} from "react";
 
 
 const Todo = () => {
 
+    //デフォルトのTodoリスト
+    const todoList = [
+        {
+            id: 1,
+            content: "店を予約する",
+            editing: false,
+        },
+        {
+            id: 2,
+            content: "買い物",
+            editing: false,
+        },
+        {
+            id: 3,
+            content: "郵便局へ行く",
+            editing: false,
+        }
+    ];
+
+    //Todoリストコンポーネントで状態をもたせる
+    const [todos, setTodos] = useState(todoList);
+
+    
     return (
         <>
             <List />
